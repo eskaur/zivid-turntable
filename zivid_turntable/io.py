cae411a6560163bd697109e999cf0b2bcca97bc7
Eskil Aursand <eskil.aursand@zivid.com>
2020-09-16 11:19:52 +0200

    Added function for manual capture loop with data storage

--- zivid_turntable/io.py
+++ zivid_turntable/io.py
@@ -1,7 +1,36 @@
 """Module for input/output"""
 
 import struct
+from pathlib import Path
 import numpy as np
+
+
+def _make_casedir_name(label: str) -> str:
+    """Create case directory name based on a label
+
+    Arguments:
+        label   Label for case directory
+    Returns:
+        Name of case directory
+    """
+    return f"data_{label}"
+
+
+def create_casedir(label: str, workdir: Path) -> Path:
+    """Create case directory based on label
+
+    Arguments:
+        label:      Label to use for directory
+        workdir:    Root directory to create case directory in
+    Returns:
+        Path to newly created case directory
+    """
+    dirpath = workdir / _make_casedir_name(label)
+    if dirpath.is_dir():
+        raise RuntimeError(f"Directory already exists: {dirpath}")
+    dirpath.mkdir()
+    print(f"Created case directory: {dirpath}")
+    return dirpath
 
 
 def write_ply_binary(fname: str, pts: np.ndarray) -> None:
